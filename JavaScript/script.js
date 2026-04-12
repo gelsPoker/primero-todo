574bad36fd3d436976282f130c22a1b7aca8bd75
Ignacio <i.bravosalgado@uandresbello.edu>
2026-04-12 06:08:45 -0400

    lol

---
+++ JavaScript/script.js
@@ -0,0 +1,21 @@
+recipiente = "ignacios";
+alert(recipiente); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  alert("Hola Mundo");    ----> Nos permite mostrar el mensaje que queramos en una ventana emergente.
+//  console.log("Hola Mundo");  ----> Nos permite mostrar el mensaje que queramos en la consola del navegador.
+//  document.write("Hola Mundo");  ----> Nos permite mostrar el mensaje que queramos en el documento HTML.
