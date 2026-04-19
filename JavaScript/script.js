9e22c83d3cae6885200f0e1086eb1f2c834febee
Ignacio <i.bravosalgado@uandresbello.edu>
2026-04-19 06:47:58 -0400

    alfo

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,5 @@
-recipiente = "ignacios";
-alert(recipiente); 
+let nombre = prompt("¿Cuál es tu nombre?");  
+alert("Hola " + nombre);
 
 
 
@@ -19,3 +19,14 @@ alert(recipiente);
 //  alert("Hola Mundo");    ----> Nos permite mostrar el mensaje que queramos en una ventana emergente.
 //  console.log("Hola Mundo");  ----> Nos permite mostrar el mensaje que queramos en la consola del navegador.
 //  document.write("Hola Mundo");  ----> Nos permite mostrar el mensaje que queramos en el documento HTML.
+
+// const nombre = "Juan";  ----> la varable nunca cambia su valor.
+// let nombre = "Juan";  ----> la variable puede cambiar su valor.
+// var nombre = "Juan";  ----> la variable puede cambiar su valor, pero es una formaantigua de declarar variables, se recomienda usar let o const.
+
+// let nombre = prompt("¿Cuál es tu nombre?");  ----> Nos permite pedir al usuario que ingrese un valor, y ese valor se guarda en la variable nombre.
+
+// nombre = "Ignacio Laubras";
+// frase = `soy ${nombre} y estoy caminando`;
+
+
